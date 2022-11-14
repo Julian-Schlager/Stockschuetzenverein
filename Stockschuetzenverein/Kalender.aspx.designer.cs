cb6bf29aefef880e425673ddee1b6a708c626cd1
florian.kastinger <florian.kastinger@htlvb.at>
2022-11-14 10:15:20 +0100

    Kalender

---
+++ Stockschuetzenverein/Kalender.aspx.designer.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Stockschuetzenverein
+{
+
+
+    public partial class Kalender
+    {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
