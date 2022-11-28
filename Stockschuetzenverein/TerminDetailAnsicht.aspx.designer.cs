5db06c30b8520f84e1397f9c279ced34f5220065
Julian Schlager <Julian.SCHLAGER@htlvb.at>
2022-11-28 10:27:50 +0100

    CSS Link Hinzugefügt, LinkButton für Icons

--- Stockschuetzenverein/TerminDetailAnsicht.aspx.designer.cs
+++ Stockschuetzenverein/TerminDetailAnsicht.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace Stockschuetzenverein
         protected global::System.Web.UI.WebControls.Button btn_addPictures;
 
         /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+
+        /// <summary>
         /// btn_editEntry control.
         /// </summary>
         /// <remarks>
