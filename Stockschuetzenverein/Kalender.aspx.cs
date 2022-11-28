34dcb64d2af3e411f8c10ad42bcd119127c8c636
Lukas.Bijelic <Lukas.Bijelic@htlvb.at>
2022-11-28 09:10:39 +0100

    using System.Web.Configuration

--- Stockschuetzenverein/Kalender.aspx.cs
+++ Stockschuetzenverein/Kalender.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 using System.Collections;
+using System.Web.Configuration;
 
 namespace Stockschuetzenverein
 {
