621d8963ce3afaaa1f4b22a2891f4bebb648f99a
Lukas.Bijelic <Lukas.Bijelic@htlvb.at>
2023-01-16 08:45:55 +0100

    OK der Aspx Kalender ist fürst erste wieder da. Ich und Kastinger werden mal bei ihm weiterarbeiten während Schlager bei eine andere Branch sich um den Bootstrap Kalender kümmern. Damn ich hoffe mal das ich Deutsch schreiben denn ich müde sehr -Lugas

--- Stockschuetzenverein/Kalender.aspx.designer.cs
+++ Stockschuetzenverein/Kalender.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace Stockschuetzenverein
         protected global::System.Web.UI.WebControls.LinkButton btn_addEntry;
 
         /// <summary>
+        /// calendar_1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar calendar_1;
+
+        /// <summary>
         /// tbl_entries control.
         /// </summary>
         /// <remarks>
