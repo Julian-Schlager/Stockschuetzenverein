2a2980342729853d23bb782db26fde365cebbd66
florian.kastinger <florian.kastinger@htlvb.at>
2023-01-25 10:28:09 +0100

    die hash und salt scheiße und dann die resultierende kacke in die DB inserten. Meine motivation ist unermesslich

--- Stockschuetzenverein/AdminRegistrierung.aspx.cs
+++ Stockschuetzenverein/AdminRegistrierung.aspx.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DataBaseWrapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,6 +11,8 @@ namespace Stockschuetzenverein
 {
     public partial class AdminRegistrierung : System.Web.UI.Page
     {
+        static string connStrg = WebConfigurationManager.ConnectionStrings["AppDbInt"].ConnectionString;
+        DataBase db = new DataBase(connStrg);
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +20,24 @@ namespace Stockschuetzenverein
 
         protected void btn_reg_Click(object sender, EventArgs e)
         {
+            string username = txt_username.Text;
+            string password = txt_password.Text;
+            string email = txt_email.Text;            
+            bool isAdmin = true;
+            string salt = Passwort.CreateSalt();
+            string hash = Passwort.CreatePasswordHash(password, salt);
 
+            try
+            {
+                string sql = $"INSERT INTO ssv_user (Username,Password,Salt,Email,IsAdmin) Values ('{username}','{hash}','{salt}','{email}','{isAdmin}')";
+                db.RunQuery(sql);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
         }
     }
 }
