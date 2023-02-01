e2c4807b65f38746f3cd44a474ca894c68fd6d1a
Julian Schlager <Julian.SCHLAGER@htlvb.at>
2023-02-01 10:14:52 +0100

    Login and Registration logic

--- Stockschuetzenverein/AdminRegistrierung.aspx.designer.cs
+++ Stockschuetzenverein/AdminRegistrierung.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace Stockschuetzenverein
         protected global::System.Web.UI.WebControls.TextBox txt_email;
 
         /// <summary>
+        /// lbl_error control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_error;
+
+        /// <summary>
         /// btn_reg control.
         /// </summary>
         /// <remarks>
